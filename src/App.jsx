d27fb8c577b622c70f0ec47af2467144b0d5002a
perugi <dominik.perusko@gmail.com>
2024-07-11 11:13:31 +0200

    Add some react jsx documentation challenges

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <p>Hello, World!</p>
-    </>
+    <div>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Greeting from "./Greeting.jsx";
+// import Greeting from "./Greeting.jsx";
+// import App from "./App.jsx";
+import Bio from "./Bio.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Greeting />
+    <Bio />
   </React.StrictMode>
 );
