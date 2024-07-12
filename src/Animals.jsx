e27c9a64b0641058dc503d4407ffab67119bcf32
perugi <dominik.perusko@gmail.com>
2024-07-12 22:54:39 +0200

    Final examples on conditional rendering

--- src/Animals.jsx
+++ src/Animals.jsx
@@ -1,22 +1,30 @@
 function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
   return (
     <ul>
-      {props.animals.map((animal) => 
-        animal.startsWith("C") ? <li>{animal}</li> : null)}
+      {props.animals.map((animal) => (
+        <li key={animal}>{animal}</li>
+      ))}
     </ul>
-  )
+  );
 }
 
 function Animals() {
-  const animals = ["Cow", "Dog", "Pelican", "Cat", "Mongoose"
-  ];
+  // const animals = ["Cow", "Dog", "Pelican", "Cat", "Mongoose"];
 
   return (
     <div>
       <h1>Animals</h1>
       <List animals={animals} />
     </div>
-  )
+  );
 }
 
 export default Animals;
